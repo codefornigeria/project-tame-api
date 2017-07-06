6db77d0d85bd48ebe921be6698eb4a5b21b23469
adeb6600 <ini@mustardlabs.io>
2017-07-06 15:46:49 +0100

    added sectors

--- src/services/scheme/scheme.hooks.js
+++ src/services/scheme/scheme.hooks.js
@@ -15,6 +15,13 @@ module.exports = {
   after: {
     all: [],
     find: [populate({
+      sectors:{
+        service:'sector',
+        f_key: '_id',
+         query:  {  // defaults to {} but you can specify any other options here
+            $select: ['name']
+          }
+      },
       group:{
         service:'group',
         f_key: '_id',
@@ -36,8 +43,29 @@ module.exports = {
       }
     })],
     get: [populate({
+      sectors:{
+        service:'sector',
+        f_key: '_id',
+         query:  {  // defaults to {} but you can specify any other options here
+            $select: ['name']
+          }
+      },
       group:{
         service:'group',
+        f_key: '_id',
+         query:  {  // defaults to {} but you can specify any other options here
+            $select: ['name','effects']
+          }
+      },
+        department:{
+        service:'department',
+        f_key: '_id',
+        query:  {  // defaults to {} but you can specify any other options here
+            $select: ['name']
+          }
+      },
+      antidotes:{
+        service:'antidote',
         f_key: '_id',
         
       }
