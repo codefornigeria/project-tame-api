0d8401241fc0105045cd834d5f30b16a5cab4e3b
adeb6600 <ini@mustardlabs.io>
2017-07-14 12:25:40 +0100

    check value of approval emails

--- src/hooks/send-request-email.js
+++ src/hooks/send-request-email.js
@@ -5,6 +5,7 @@ module.exports = options => hook => {
   if (!hook.params.provider) { return hook; }
   const request = hook.result
   if( hook.data  && request) { 
+    console.log('checking approval emails', hook.app.get('approvalEmails'))
     accountService(hook.app)('sendRequestMail', request, hook.app.get('approvalEmails'))
     return hook
   }
